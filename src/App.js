6b0e06cee34dfa893ba6f9b305dd6e3fa7d8fd45
Miki <ghermaico135@gmail.com>
2023-07-24 13:46:14 +0300

    Adding Settiing for Theme modes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 /** @format */
 
-import "./App.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./Themes";
+import TopBar from "./scenes/global/TopBar";
 
 function App() {
-	return <div className="app">react-admin</div>;
+	const [colorMode, theme] = useMode();
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				{/* rest css to default */}
+				<CssBaseline />
+				<div className="app">
+					<main className="content">
+						<TopBar />
+					</main>
+				</div>
+				;
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import { Box, IconButton,InputBase,useTheme } from '@mui/material'
+import React, { useContext } from 'react'
+import { ColorModeContext, tokens } from '../../Themes'
+import LightModeOutlinedIcon  from '@mui/icons-material/LightModeOutlined'
+import DarkModeOutlinedIcon  from '@mui/icons-material/DarkModeOutlined'
+import NotificationOutlinedIcon  from '@mui/icons-material/NotificationOutlined'
+import SettingsOutlinedIcon  from '@mui/icons-material/SettingsOutlined'
+import PersonOutlinedIcon  from '@mui/icons-material/PersonOutlined'
+import SearchIcon  from '@mui/icons-material/Search'
 
 const TopBar = () => {
   return (
